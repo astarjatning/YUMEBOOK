53d6148f150a6359877e398a3015b388c5c93c37
astarjatning <og.fill@gmail.com>
2022-01-03 19:15:15 +0900

    FIX:manifest.js

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,4 +1,3 @@
 //= link_tree ../images
-//= link_directory ../javascript/packs .js
-//= link_directory ../javascript/channels .js
+//= link_directory ../javascript .js
 //= link_directory ../stylesheets .css
