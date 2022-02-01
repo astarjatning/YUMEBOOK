aebdfee99eefae8aa153081707b81cbc4f43a2a8
astarjatning <og.fill@gmail.com>
2022-02-01 12:19:19 +0900

    update: toastrを別ファイルに移動,スタイル、翻訳微修正

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,25 +7,7 @@ import 'bootstrap'
 import "@fortawesome/fontawesome-free/css/all.css"
 import '../stylesheets/application'
 import '../javascripts/flatpickr'
-import toastr from 'toastr'
-window.toastr = toastr
-toastr.options = {
-  "closeButton": false,
-  "debug": false,
-  "newestOnTop": false,
-  "progressBar": false,
-  "positionClass": "toast-top-left",
-  "preventDuplicates": false,
-  "onclick": null,
-  "showDuration": "300",
-  "hideDuration": "1000",
-  "timeOut": "5000",
-  "extendedTimeOut": "1000",
-  "showEasing": "swing",
-  "hideEasing": "linear",
-  "showMethod": "fadeIn",
-  "hideMethod": "fadeOut"
-}
+import '../javascripts/toastr'
 
 
 Rails.start()
