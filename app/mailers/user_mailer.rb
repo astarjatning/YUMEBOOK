d17ea86e696833f9bac1e0aa0f06c8d18f49ba95
astarjatning <og.fill@gmail.com>
2022-03-14 23:49:14 +0900

    add:ユーザー退会

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,4 +5,16 @@ class UserMailer < ApplicationMailer
     mail(to: user.email,
          subject: t('defaults.password_reset'))
   end
+
+  def greeting_mail(user)
+    @user = User.find(user.id)
+    @url = 'https://www.yume-book.com/login'
+    mail(to: user.email,
+         subject: t('defaults.greeting_mail'))
+  end
+
+  def account_deleted(user)
+    mail(to: user.email,
+         subject: t('defaults.account_deleted'))
+  end
 end
